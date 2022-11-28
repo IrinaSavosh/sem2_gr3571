2564fb381e805ce58539a648b0d719f434fa9371
IrinaSavosh <irina_savosh@mail.ru>
2022-11-28 14:00:12 +0300

    Task: a multiple of two numbers

---
+++ 14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23
+// 14 - нет
+// 46 - нет
+// 161 - да
+
+Console.WriteLine("Введите число a:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int mod = numberA % 7;
+int dom = numberA % 23;
+if(mod == 0 && dom == 0)
+{
+   Console.WriteLine("кратно");
+}
+else
+{
+   Console.WriteLine("не кратно");
+}
