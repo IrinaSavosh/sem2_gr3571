1e3211ebeddc38aa67272c772c2a8fb6d7baf32e
IrinaSavosh <irina_savosh@mail.ru>
2022-11-28 13:57:15 +0300

    Task: multiplicity of numbers

---
+++ 12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на ввод два числа
+// и выводить, является ли первое число кратно второму.
+// Если число 2 не является кратным 1, то программа выводит остаток от деления
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+
+Console.WriteLine("Введите число a:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int mod = numberA % numberB;
+if(mod == 0)
+{
+   Console.WriteLine("кратно");
+}
+else
+{
+   Console.WriteLine($"не кратно, остаток {mod}");
+}
